cf68c2c4ea085e0c191f70f4f5c6c07f774f0437
crsegerie <charbel-raphael.segerie@hotmail.fr>
2021-10-07 22:53:44 +0200

    provisoire commit

--- action.py
+++ action.py
@@ -1,16 +1,33 @@
 import pandas as pd
-
+from utils import Difficulty
 
 class Action:
     """Defining the possible actions for the agent
     in its environment.
     """
 
-    def __init__(self) -> None:
-        self.actions = pd.DataFrame([
-            {"name": "walking_right", "definition": 0, "constraint": 0, "control": 0, "coefficient loss": 0},
+    def __init__(self, difficulty: Difficulty) -> None:
+        
+        def wlaking_constrain : 
+            return None
+        
+        
+        
+        actions_list = [
+            {"name": "walking_right", "definition": 0, "constraint": wlaking_constrain, "control": 0, "coefficient loss": 0},
             {"name": "walking_left", "definition": 0, "constraint": 0, "control": 0, "coefficient loss": 0},
             {"name": "walking_up", "definition": 0, "constraint": 0, "control": 0, "coefficient loss": 0},
-            {"name": "walking_down", "definition": 0, "constraint": 0, "control": 0, "coefficient loss": 0}])
+            {"name": "walking_down", "definition": 0, "constraint": 0, "control": 0, "coefficient loss": 0}
+            ]
+        
+        constraints_eating = [lambda (i, x) :  ]
+        
+        actions_list = actions_list + \
+            [{"name": f"eat resource {str(resource)}", "definition": 0, "constraint": lambda , "control": 0, "coefficient loss": 0} 
+             for resource in range(difficulty.n_resources)]
+        
+        
+        
+        self.actions = pd.DataFrame(actions_list)
 
         self.n_actions = len(self.actions)
